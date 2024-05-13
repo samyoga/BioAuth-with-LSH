b5683619423adcc040ab82c47b5a8335993dbc8a
samyoga <bhattaraisamyoga@gmail.com>
2024-05-13 17:58:58 -0500

    generate random matrices

--- facerecognitionsystem.py
+++ facerecognitionsystem.py
@@ -1,6 +1,7 @@
 from zipfile import ZipFile
 import cv2
 import json
+import numpy as np
 
 # function to extract zip files
 def extract_images(zip_file):
@@ -19,6 +20,24 @@ def enroll_user(image_data):
     
     return features_dict
 
+# function to hash the templates
+def hash_templates():
+    
+    return 0
+
+# function to generate random matrices
+def generate_random_matrices(num_matrices, matrix_size):
+    #Initialize a list to store matrices
+    random_matrices = []
+
+    for _ in range(num_matrices):
+        # Generate a random matrix with matrix_size
+        # Each element of the matrix is a list with 3 random elements
+        random_matrix = np.random.rand(matrix_size, matrix_size, 3)
+        random_matrices.append(random_matrix)
+
+    return random_matrices
+
 # function to verify users
 def verify_user():
     return 0
@@ -34,4 +53,5 @@ if __name__ == "__main__":
     # dump dictionary to JSON
     with open("gallery.json", "w") as outfile:
         json.dump(features_dict, outfile)
+        
     probe_data = "ProbeSet"
