c1a69adaa31b116c808006415ac20bdafb498538
samyoga <bhattaraisamyoga@gmail.com>
2024-05-15 13:34:03 -0500

    Generate random matrices and dump it to a json file

--- facerecognitionsystem.py
+++ facerecognitionsystem.py
@@ -9,7 +9,7 @@ def extract_images(zip_file):
         zip_extract.extractall()
 
 # function to enroll users
-def enroll_user(image_data):
+def enroll_user():
     features_dict = {}
     for i in range(1, 101):
         #read images from the gallery set
@@ -17,26 +17,41 @@ def enroll_user(image_data):
         if i not in features_dict:
             features_dict[i] = []
         features_dict[i].append(images_gallery.tolist())
-    
     return features_dict
 
 # function to hash the templates
-def hash_templates():
+def hash_templates(input_array, random_matrices):
+    dot_products = []
     
-    return 0
+    for random_matrix in random_matrices:
+        product = np.dot(random_matrix, input_array)
+        dot_products.append(product)
+    
+    return dot_products
 
 # function to generate random matrices
 def generate_random_matrices(num_matrices, matrix_size):
     #Initialize a list to store matrices
     random_matrices = []
+    random_keys = []
+    my_dict = {}
+
+    for i in range(1, num_matrices+1):
+        random_keys.append(i)
 
     for _ in range(num_matrices):
         # Generate a random matrix with matrix_size
         # Each element of the matrix is a list with 3 random elements
-        random_matrix = np.random.rand(matrix_size, matrix_size, 3)
-        random_matrices.append(random_matrix)
+        random_matrix = np.random.randint(-100, 101, size=(matrix_size, matrix_size, 3))  
+        random_matrices.append(random_matrix.tolist())
 
-    return random_matrices
+    
+    
+    # print(random_keys)
+    key_value_pairs = zip(random_keys, random_matrices)
+    my_dict = dict(key_value_pairs)
+    print(my_dict)
+    return my_dict
 
 # function to verify users
 def verify_user():
@@ -47,11 +62,17 @@ if __name__ == "__main__":
     extract_images("GallerySet.zip")
     extract_images("ProbeSet.zip")
 
-    gallery_data = "GallerySet"
-    features_dict = enroll_user(gallery_data)
+    features_dict = enroll_user()
+
+    num_matrices = 255
+    matrix_size = 50
+    random_matrices = generate_random_matrices(num_matrices, matrix_size)
+    # hash_templates(features_dict, random_matrices)
     
     # dump dictionary to JSON
     with open("gallery.json", "w") as outfile:
         json.dump(features_dict, outfile)
-        
+
+    with open("random_matrices.json", "w") as outfile:
+        json.dump(random_matrices, outfile)
     probe_data = "ProbeSet"
