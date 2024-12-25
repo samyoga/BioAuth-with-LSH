97a58b31e0039e1d731e7c7df9ed525c680b64aa
samyoga <bhattaraisamyoga@gmail.com>
2024-12-24 18:51:24 -0600

    refactor codebase to create class for hamming distance similarity computation

---
+++ Classes/similarity_calculator.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class SimilarityCalculator:
+    def compute_similarity(probe_vector, gallery_vector):
+        """ 
+        Compute similarity between two vectors using hamming distance
+        """
+        
+        # Truncate vectors to the length of the shorter one
+        min_length = min(len(probe_vector), len(gallery_vector))
+        probe_array = np.array(probe_vector[:min_length])
+        gallery_array = np.array(gallery_vector[:min_length])
+
+        # Calculate Hamming distance
+        distance = np.sum(probe_array != gallery_array)
+
+        # Compute similarity as 1-(distance/total_bits)
+        similarity = 1 - (distance / min_length)
+
+        return similarity
